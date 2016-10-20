c733f6998184cf4847d9558c6f4dc83cf579bc18
Jason Hanna <jason.hanna01@gmail.com>
2016-10-20 08:24:03 -0600

    add enemy, player, and powerup tests

---
+++ test/enemy-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Enemy = require('../lib/enemy');
+
+describe('Enemy', function() {
+  context('with default attributes', function() {
+    var enemy = new Enemy(400, 550, 10, 1, 3 );
+
+    it('should assign an x coordinate', function() {
+      assert.equal(enemy.x, 400);
+    });
+    it('should assign an y coordinate', function() {
+      assert.equal(enemy.y, 550);
+    });
+    it('should assign a radius', function() {
+      assert.equal(enemy.radius, 10);
+    });
+    it('should assign an xspeed', function() {
+      assert.equal(enemy.xspeed, 1);
+    });
+    it('should assign a yspeed', function() {
+      assert.equal(enemy.yspeed, 3);
+    });
+  });
+});
--- test/player-test.js
+++ test/player-test.js
@@ -4,16 +4,16 @@ const Player = require('../lib/player');
 
 describe('Player', function() {
   context('with default attributes', function() {
-    var player = new Player();
+    var player = new Player(400, 550, 10);
 
     it('should assign an x coordinate', function() {
-      assert.equal(player.x, 400)
-    })
+      assert.equal(player.x, 400);
+    });
     it('should assign an y coordinate', function() {
-      assert.equal(player.y, 550)
-    })
+      assert.equal(player.y, 550);
+    });
     it('should assign a radius', function() {
-      assert.equal(player.radius, 10)
-    })
-  })
-})
+      assert.equal(player.radius, 10);
+    });
+  });
+});
