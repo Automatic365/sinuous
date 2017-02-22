27191369dfc161c4388481ba89469b7c7da7b68e
Jason Hanna <jason.hanna01@gmail.com>
2017-02-21 21:52:01 -0700

    Update main.bundle.js

--- main.bundle.js
+++ main.bundle.js
@@ -56,7 +56,7 @@
 	  // context.fillText("_______", 290, 176);
 	  context.font = '20px Exo';
 	  context.fillText("Directions:", 175, 275);
-	  context.fillText("* Avoid red enemy dots and the grey boarder", 175, 325);
+	  context.fillText("* Avoid red enemy dots and the grey border", 175, 325);
 	  context.fillText("* Collide with green dots for +1 life", 175, 360);
 	  context.fillText("* Place your cursor so that it is hidden inside the boarder", 175, 395);
 	  context.fillText("* Use your trackpad or mouse to move the blue player ball", 175, 430);
